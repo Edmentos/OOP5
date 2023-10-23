3e98c089926d610b7c1a238e0ee439cc3f356038
Edmond <G00414989@atu.ie>
2023-10-23 17:15:12 +0100

    Added Scanner

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -4,8 +4,10 @@ package ie.atu;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        //Create a new person object
         Person student1 = new Person("Edmond", "Wilkinson", 19);
 
+        //Call the display info method
+        student1.displayInfo();
         }
     }
--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,17 +1,11 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Person {
     private String firstName;
     private String lastName;
     private int age;
-
-
-
-    public Person() {
-        this.firstName = "";
-        this.lastname = "";
-        this.age = 0;
-    }
 
 
     public Person(String firstName, String lastName, int age) {
@@ -20,9 +14,34 @@ public class Person {
         this.age = age;
     }
 
-    public void displayInfo() {
-        System.out.println("Name: " + firstName + " " + lastName + ", Age " + age);
+    public Person() {
+        this.firstName = "";
+        this.lastName = "";
+        this.age = 0;
     }
+
+    public void getUserInput(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first name: ");
+        this.firstName = scanner.nextLine();
+
+        System.out.print("Enter last name: ");
+        this.lastName = scanner.nextLine();
+
+        System.out.print("Enter age ");
+        this.age = scanner.nextInt();
+    }
+
+
+
+
+
+
+
+
+
+
 
     public String getFirstName() {
         return firstName;
@@ -47,4 +66,8 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+    public void displayInfo() {
+        System.out.println("Name: " + firstName + " " + lastName + ", Age " + age);
+    }
+
 }
